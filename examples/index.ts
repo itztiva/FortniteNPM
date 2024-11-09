033eab3248db128f52455b808836174041ebe2c4
Itztiva <tiva@Itztiva.com>
2024-11-09 17:37:01 -0500

    forgot

--- examples/index.ts
+++ examples/index.ts
@@ -2,7 +2,7 @@ import { Fortnite } from "../src";
 
 export const fortnite = new Fortnite();
 
-const code = 'authorization code'; // get from <https://www.epicgames.com/id/api/redirect?clientId=3f69e56c7649492c8cc29f1af08a8a12&responseType=code>
+const code = 'authorizationcode'; // get from <https://www.epicgames.com/id/api/redirect?clientId=3f69e56c7649492c8cc29f1af08a8a12&responseType=code>
 
 (async () => {
     try {
@@ -11,6 +11,6 @@ const code = 'authorization code'; // get from <https://www.epicgames.com/id/api
         const athena = await fortnite.getProfile();
         await Bun.write('profile.json', JSON.stringify(athena, null, 2));
     } catch (error) {
-        console.error('Login failed:', error);
+        console.error('login failed:', error);
     }
 })();
